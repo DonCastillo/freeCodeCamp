9497e0f590f78f92cf83263c6b151af49257195d
DonCastillo <don.qcastillo@yahoo.com>
2021-02-15 16:08:04 -0700

    Create 002_sort.js

---
+++ 001_js/002_sort.js
@@ -0,0 +1,13 @@
+/**
+ * Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+ */
+
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+
+  return arr.sort();
+
+  // Only change code above this line
+}
+const sorted = alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+console.log(sorted);

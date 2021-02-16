a946d1f50f41e37aa6698bf83f6c360f09123d67
DonCastillo <don.qcastillo@yahoo.com>
2021-02-15 17:06:14 -0700

    finished url-slug

--- 001_js/004_split.js
+++ 001_js/004_split.js
@@ -4,7 +4,7 @@
 
 function splitify(str) {
 	// Only change code below this line
-    return str.split(/\W/);
+	return str.split(/\W/);
 	// Only change code above this line
 }
 console.log(splitify("Hello World,I-am code"));

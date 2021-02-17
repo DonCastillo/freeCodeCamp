fc5382cb1e4c3f1a0eab82d57055ddebb800a386
DonCastillo <don.qcastillo@yahoo.com>
2021-02-17 00:30:03 -0700

    Create 029_make_a_person.js

---
+++ 001_js/029_make_a_person.js
@@ -0,0 +1,50 @@
+/**
+ * Fill in the object constructor with the following methods below:
+ * getFirstName()
+ * getLastName()
+ * getFullName()
+ * setFirstName(first)
+ * setLastName(last)
+ * setFullName(firstAndLast)
+ * Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+ */
+
+var Person = function (firstAndLast) {
+	// Only change code below this line
+
+	this.getFirstName = function () {
+		return firstAndLast.split(" ")[0];
+	};
+	this.getLastName = function () {
+		return firstAndLast.split(" ")[1];
+	};
+	this.setFirstName = function (str) {
+		let name = firstAndLast;
+		name = name.split(" ");
+		name.shift();
+		name.unshift(str);
+		firstAndLast = name.join(" ");
+	};
+	this.setLastName = function (str) {
+		let name = firstAndLast;
+		name = name.split(" ");
+		name.pop();
+		name.push(str);
+		firstAndLast = name.join(" ");
+	};
+	this.setFullName = function (str) {
+		firstAndLast = str;
+	};
+	// Complete the method below and implement the others similarly
+	this.getFullName = function () {
+		return firstAndLast;
+	};
+	return firstAndLast;
+};
+
+var bob = new Person("Bob Ross");
+console.log(bob.getFullName());
+bob.setFirstName("Haskell");
+console.log(bob.getFullName());
+bob.setLastName("Curry");
+console.log(bob.getFullName());

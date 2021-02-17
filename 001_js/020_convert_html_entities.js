8d00508f189c00984485ce308eecf919e947e1a0
DonCastillo <don.qcastillo@yahoo.com>
2021-02-16 19:22:53 -0700

    Create 020_convert_html_entities.js

---
+++ 001_js/020_convert_html_entities.js
@@ -0,0 +1,20 @@
+/**
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+ */
+
+function convertHTML(str) {
+	str = str.replace(/&/g, "&amp;");
+	str = str.replace(/</g, "&lt;");
+	str = str.replace(/>/g, "&gt;");
+	str = str.replace(/"/g, "&quot;");
+	str = str.replace(/'/g, "&apos;");
+	return str;
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+console.log(convertHTML("Sixty > twelve"));
+console.log(convertHTML('Stuff in "quotation marks"'));
+console.log(convertHTML("Schindler's List"));
+console.log(convertHTML("<>"));
+console.log(convertHTML("abc"));
